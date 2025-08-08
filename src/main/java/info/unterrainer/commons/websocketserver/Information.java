e6d85c9bdaf1b21071f6dcc1614f337dedcecf3d
Gerald <gerald@unterrainer.info>
2025-08-08 08:24:04 +0200

    update

--- src/main/java/info/unterrainer/commons/websocketserver/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.commons.websocketserver;
-
-public class Information {
-	public static final String name = "Websocket-Server";
-	public static final String buildTime = "2025-07-25T13:50:51Z";
-	public static final String pomVersion = "1.0.0";
-}
